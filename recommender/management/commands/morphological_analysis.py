c63b593ab9266118b3997c2a3b7fa3196fadd58c
xim0608 <xim.ryuki@gmail.com>
2018-09-24 11:41:38 +0900

    exception process and notification

--- recommender/management/commands/morphological_analysis.py
+++ recommender/management/commands/morphological_analysis.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from recommender.lib.morphological_analysis import *
 import time
+from socket import gethostname
+
 
 class Command(BaseCommand):
     help = 'import spot and review data to mongodb'
@@ -13,10 +15,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         start = time.time()
-        if options['method'] == 'mecab':
-            AnalysisMecab().analysis_and_save()
-        elif options['method'] == 'jumanpp':
-            AnalysisJuman().analysis_and_save()
+        try:
+            if options['method'] == 'mecab':
+                AnalysisMecab().analysis_and_save()
+            elif options['method'] == 'jumanpp':
+                AnalysisJuman().analysis_and_save()
+        except Exception as e:
+            import traceback
+            from recommender.lib.notifications import Slack
+            print(traceback.print_exc())
+            Slack.notify("<!channel>\n{}\nScript has stopped in {}".format(str(type(e)), gethostname()))
 
         elapsed_time = time.time() - start
         print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
