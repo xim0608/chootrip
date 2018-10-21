7d1623183701f781884e86969482933ea673faf4
xim0608 <xim.ryuki@gmail.com>
2018-10-21 12:42:55 +0900

    fix json dump

--- recommender/management/commands/json_dump.py
+++ recommender/management/commands/json_dump.py
@@ -9,30 +9,52 @@ import json
 class Command(BaseCommand):
     help = 'create data frame'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--method', dest='method', required=True,
+            help='morphological analysis method',
+        )
+
     def handle(self, *args, **options):
         start = time.time()
 
         spots = Spot.objects.all()
-        created_at = datetime.now()
         json_data = {}
         data_count = 1
-        for spot in spots:
-            contents = []
-            a_r = AnalyzedReview.objects.filter(review__spot__id=spot.id).only('neologd_title', 'neologd_content')
-            for r in a_r:
-                contents.append(r.neologd_title)
-                contents.append(r.neologd_content)
-            json_data[spot.id] = contents
-            if spot.id % 1000 == 0:
-                file_path = settings.BASE_DIR + "/recommender/lib/files/jsons/{}_{}.json".format(
-                    created_at.strftime('%Y%m%d%H%M%S'), data_count)
-                f = open(file_path, 'w')
-                json.dump(json_data, f)
-                json_data = {}
-                data_count += 1
-        file_path = settings.BASE_DIR + "/recommender/lib/files/jsons/{}_{}.json".format(
-            datetime.now().strftime('%Y%m%d%H%M%S'), data_count)
-        f = open(file_path, 'w')
-        json.dump(json_data, f)
+
+        if options['method'] == 'neologd':
+            for spot in spots:
+                contents = []
+                a_r = AnalyzedReview.objects.filter(review__spot__id=spot.id).only('neologd_title', 'neologd_content')
+                for r in a_r:
+                    contents.append(r.neologd_title)
+                    contents.append(r.neologd_content)
+                json_data[spot.id] = contents
+                if spot.id % 5000 == 0:
+                    file_path = settings.BASE_DIR + "/recommender/lib/files/jsons/neologd/{}.json".format(data_count)
+                    f = open(file_path, 'w')
+                    json.dump(json_data, f)
+                    json_data = {}
+                    data_count += 1
+            file_path = settings.BASE_DIR + "/recommender/lib/files/jsons/neologd/{}.json".format(data_count)
+            f = open(file_path, 'w')
+            json.dump(json_data, f)
+        elif options['method'] == 'jumanpp':
+            for spot in spots:
+                contents = []
+                a_r = AnalyzedReview.objects.filter(review__spot__id=spot.id).only('jumanpp_title', 'jumanpp_content')
+                for r in a_r:
+                    contents.append(r.jumanpp_title)
+                    contents.append(r.jumanpp_content)
+                json_data[spot.id] = contents
+                if spot.id % 5000 == 0:
+                    file_path = settings.BASE_DIR + "/recommender/lib/files/jsons/jumanpp/{}.json".format(data_count)
+                    f = open(file_path, 'w')
+                    json.dump(json_data, f)
+                    json_data = {}
+                    data_count += 1
+            file_path = settings.BASE_DIR + "/recommender/lib/files/jsons/jumanpp/{}.json".format(data_count)
+            f = open(file_path, 'w')
+            json.dump(json_data, f)
         elapsed_time = time.time() - start
         print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
