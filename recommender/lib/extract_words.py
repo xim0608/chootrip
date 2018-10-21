c1cf61e8d0772981f14176b297441a7af9e76d1b
xim0608 <xim.ryuki@gmail.com>
2018-10-21 22:46:07 +0900

    add stop words

--- recommender/lib/extract_words.py
+++ recommender/lib/extract_words.py
@@ -1,4 +1,5 @@
 import re
+import urllib
 
 
 def include_num(word):
@@ -7,10 +8,27 @@ def include_num(word):
     return False
 
 
+def __get_sloth_lib_stop_words():
+    slothlib_path = 'http://svn.sourceforge.jp/svnroot/slothlib/CSharp/Version1/SlothLib/NLP/Filter/StopWord/word/Japanese.txt'
+    slothlib_file = urllib.request.urlopen(slothlib_path)
+    slothlib_stopwords = [line.decode("utf-8").strip() for line in slothlib_file if line.decode("utf-8").strip() != '']
+    return slothlib_stopwords
+
+
+def get_stop_words():
+    stopwords = ['\n', '、', '「', '」', '（', '）']
+    stopwords += __get_sloth_lib_stop_words()
+    print('created stopwords_list')
+    return stopwords
+
+
+stopwords = get_stop_words()
+
+
 def extract_neologd_nouns(review):
     review_words = []
     for node_with_feature in review:
-        if node_with_feature[0] == '名詞' and not include_num(node_with_feature[6]):
+        if node_with_feature[0] == '名詞' and not include_num(node_with_feature[6]) and not node_with_feature[6] in stopwords:
             if node_with_feature[1] == '一般' or node_with_feature[1] == 'サ変接続' or \
                     (node_with_feature[1] == '固有名詞' and not node_with_feature[2] == '人名' and not node_with_feature[2] == '地域'):
                 review_words.append(node_with_feature[6])
