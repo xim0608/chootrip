b5039ed3d9df1bd1a5f150a08ff7c69f6098ff27
xim0608 <xim.ryuki@gmail.com>
2018-10-21 09:31:20 +0900

    fix create topic model script

--- recommender/management/commands/create_topic_model.py
+++ recommender/management/commands/create_topic_model.py
@@ -3,16 +3,15 @@ import time
 from socket import gethostname
 from datetime import datetime
 from recommender.lib.models import TopicModel, Corpus
+from django.conf import settings
+import glob
+import os
 
 
 class Command(BaseCommand):
     help = 'import spot and review data to mongodb'
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            '--topic-model', dest='topic_model_created_at', required=False,
-            help='model created_at',
-        )
         parser.add_argument(
             '--corpus', dest='corpus_created_at', required=False,
             help='model created_at',
@@ -21,24 +20,27 @@ class Command(BaseCommand):
             '--num_topics', dest='num_topics', required=True,
             help='num of topics',
         )
-        parser.add_argument(
-            '--load_json', dest='load_json', required=False,
-            help='num of topics',
-        )
+
+    def get_latest_saved_corpus(self):
+        ls = glob.glob(settings.BASE_DIR + "/recommender/lib/corpus/*")
+        times = [os.path.getctime(file) for file in ls]
+        idx = times.index(max(times))
+        latest_dir_name = ls[idx]
+        latest_dir_time = latest_dir_name.split('/')[-1]
+        return latest_dir_time
 
     def handle(self, *args, **options):
         start = time.time()
         try:
-            if options['topic_model_created_at']:
-                created_at = datetime.strptime(options['topic_model_created_at'], '%Y%m%d%H%M%S')
-                TopicModel(num_topics=options['num_topics'], created_at=created_at)
+            if options['corpus_created_at']:
+                created_at = datetime.strptime(options['corpus_created_at'], '%Y%m%d%H%M%S')
+                corpus = Corpus(created_at=created_at)
+                TopicModel(num_topics=options['num_topics'], corpus=corpus)
             else:
-                if options['corpus_created_at']:
-                    created_at = datetime.strptime(options['corpus_created_at'], '%Y%m%d%H%M%S')
-                    corpus = Corpus(created_at=created_at)
-                    TopicModel(num_topics=options['num_topics'], corpus=corpus)
-                else:
-                    TopicModel(num_topics=options['num_topics'], corpus=Corpus())
+                # get latest corpus
+                created_at = datetime.strptime(self.get_latest_saved_corpus(), '%Y%m%d%H%M%S')
+                corpus = Corpus(created_at=created_at)
+                TopicModel(num_topics=options['num_topics'], corpus=corpus)
         except Exception as e:
             import traceback
             from recommender.lib.notifications import Slack
