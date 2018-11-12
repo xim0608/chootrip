405f47e1d71211647484608aa19ee88022a933a1
xim0608 <xim.ryuki@gmail.com>
2018-11-12 09:46:13 +0900

    delete some dead codes

--- recommender/serializer.py
+++ recommender/serializer.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers, pagination
+from rest_framework import serializers
 from .models import Spot, SpotImage
 
 
@@ -9,17 +9,10 @@ class SpotImageSerializer(serializers.ModelSerializer):
 
 
 class SpotSerializer(serializers.ModelSerializer):
-    # city = Serializer()
-    # image = serializers.SerializerMethodField()
-    # image = SpotImageSerializer(many=True, read_only=True)
-
-    # def get_image(self, instance):
-    #     return SpotImageSerializer(instance.spotimage_set.first()).data
     spotimage_set = SpotImageSerializer(read_only=True, many=True)
 
     class Meta:
         model = Spot
-        # fields = ('id', 'base_id', 'title', 'url', 'city')
         fields = ('id', 'title', 'url', 'spotimage_set')
         ordering_fields = ('count', '-count',)
         ordering = ('-count',)
