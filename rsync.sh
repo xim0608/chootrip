8b4b6c04cb908adc8927bdbcf5573524c8e41f2f
xim0608 <xim.ryuki@gmail.com>
2018-11-20 12:51:16 +0900

    fix error

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync -r ./recommender/lib/files/corpus batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/corpus
-rsync -r ./recommender/lib/files/similarities_index batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/similarities_index
-rsync -r ./recommender/lib/files/topic_model batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/topic_model
+rsync -av -P -r ./recommender/lib/files/corpus/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/corpus
+rsync -av -P -r ./recommender/lib/files/similarities_index/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/similarities_index
+rsync -av -P -r ./recommender/lib/files/topic_model/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/topic_model
