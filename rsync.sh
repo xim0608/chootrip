b24361decdef3616370d1b34670aa04891a9c417
xim0608 <xim.ryuki@gmail.com>
2018-11-20 14:07:36 +0900

    fix

--- rsync.sh
+++ rsync.sh
@@ -2,4 +2,4 @@
 
 rsync -av -P -r ./recommender/lib/files/corpus/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/corpus
 rsync -av -P -r ./recommender/lib/files/similarities_index/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/similarities_index
-rsync -av -P -r ./recommender/lib/files/topic_model/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/topic_model
+rsync -av -P -r ./recommender/lib/files/topic_model/ batch0201.asia-northeast1-b.chootrip:/home/ryuki/chootrip/recommender/lib/files/topic_model
